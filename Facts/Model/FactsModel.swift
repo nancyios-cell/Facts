2b1f7d9aea930858506f8a3fab00672a9179dee0
Nancy Pandya <nancy.pandya@quadlogix.com>
2020-11-12 18:32:25 +0530

    modified readme file

--- Facts/Model/FactsModel.swift
+++ Facts/Model/FactsModel.swift
@@ -8,46 +8,6 @@
 import UIKit
 import SwiftUI
 
-/*"title":"About Canada",
-"rows":[
-    {
-    "title":"Beavers",
-    "description":"Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony",
-    "imageHref":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
-    }]*/
-
-/* class FactsModel: NSObject {
-    var rows = [Facts]()
-    var title = ""
-    
-    init(object:[String:AnyObject]) {
-        for dict in object["rows"] as! [[String: AnyObject]]{
-            let data2 = Facts.init(object: dict)
-            rows.append(data2)
-        }
-        title = object["title"] as! String
-    }
-
-}
-
-class Facts: NSObject {
-     var title = ""
-     var descriptions = ""
-     var imageHref = ""
-    
-    init(object:[String: AnyObject]) {
-        title = object["title"] as! String
-        descriptions = object["description"] as! String
-        imageHref = object["imageHref"] as! String
-    }
-}  */
-
-/*public protocol Identifiable {
-
-    var id: UUID {
-        return UUID()
-    }
-} */
 
 struct FactsModel:Hashable, Codable ,Identifiable{
     var id: UUID {
--- README.md
+++ README.md
@@ -2,4 +2,9 @@
  This Project is build on SwiftUI which uses the [api](https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json) to display data.
 
 ## Note:
+  [Kingfisher pod](https://github.com/onevcat/Kingfisher) is used to display the async images in SwiftUI.
+  ```bash
+  pod I'Kingfisher/SwiftUI'
+  ```
  There are few images or fields in response data which passes null values. So, SwiftUI shows the data accordingly.
+
